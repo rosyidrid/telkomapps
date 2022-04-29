9ebca7a706d8783ed3d006d117b61def9a53ff28
rosyidrid <rosyidridho9g@gmail.com>
2022-04-29 16:04:48 +0800

    photo page

--- lib/pages/photo/photo.dart
+++ lib/pages/photo/photo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class PhotoPage extends StatelessWidget {
   const PhotoPage({Key? key}) : super(key: key);
@@ -20,58 +22,64 @@ class PhotoPage extends StatelessWidget {
               children: <Widget>[
                 Container(
                   width: size.width * 0.9,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Column(
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: <Widget>[
-                            Image.asset("assets/image/jpeg/foto_1.jpg"),
-                            Text(
-                              "SALAH",
-                              style: TextStyle(
-                                  color: Colors.red,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w300),
+                            Column(
+                              children: <Widget>[
+                                Image.asset("assets/image/jpeg/foto_1.jpg"),
+                                Text(
+                                  "SALAH",
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Image.asset("assets/image/jpeg/foto_2.jpg"),
-                            Text(
-                              "BENAR",
-                              style: TextStyle(
-                                  color: Colors.green,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w300),
-                            ),
-                          ],
-                        )
-                      ]),
+                            Column(
+                              children: <Widget>[
+                                Image.asset("assets/image/jpeg/foto_2.jpg"),
+                                Text(
+                                  "BENAR",
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ],
+                            )
+                          ]),
+                      Container(
+                          width: size.width * 0.85,
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Ketentuan :",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16)),
+                              Text(
+                                  "1. Posisi hp dimiringkan\n2. Wajah dan Outlet terlihat jelas",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 14)),
+                            ],
+                          )),
+                    ],
+                  ),
                 ),
-                Container(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Ketentuan :",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20)),
-                    Text(
-                        "1. Posisi hp dimiringkan\n2. Wajah dan Outlet terlihat jelas",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 18)),
-                  ],
-                )),
                 ElevatedButton(
                   onPressed: () {},
                   child: Text("Ambil Foto"),
                   style: ElevatedButton.styleFrom(
                     primary: Color(0xFFFF4949),
-                    minimumSize: Size(size.width * 0.8, 40),
+                    minimumSize: Size(size.width * 0.85, 40),
                   ),
                 )
               ]),
