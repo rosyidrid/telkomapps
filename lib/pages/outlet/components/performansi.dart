d79c50823833ac93aa4aa8f78ef29c8d733974ae
robbyfa71 <robbyfirdauzy@gmail.com>
2022-05-11 01:41:06 +0800

    buat table

--- lib/pages/outlet/components/performansi.dart
+++ lib/pages/outlet/components/performansi.dart
@@ -17,7 +17,72 @@ class _PerformansiState extends State<Performansi> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Text("PERFORMANSI",
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18)),
+              style: TextStyle(
+              fontWeight: FontWeight.w500, fontSize: 18)),
+          Padding(
+            padding: EdgeInsets.all(10),
+          ),
+          Table(
+            border: TableBorder(
+              horizontalInside: BorderSide(
+                color: Colors.grey
+              )
+            ),
+            children: [
+              TableRow(children: [
+                Text("TAP", style:TextStyle(color: Colors.red)),
+                Text(": Value"),
+              ]),
+              TableRow(children: [
+                Text("rs_register_active", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_pjp_active", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_productive_all", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_productive_nsb", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_productive_omni", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_productive_cvm", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_productive_games", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_productive_dls", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("rs_productive_voice", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("seltru_sp", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("seltru_vf", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+              TableRow(children: [
+                Text("Usim", style:TextStyle(color: Colors.red)),
+                Text(": value"),
+              ]),
+            ],
+          )
         ],
       ),
     );
